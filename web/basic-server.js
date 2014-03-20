3699b56d7bede5ad8898806f2c7fafca2486656f
Hack Reactor Students <jontewks@gmail.com>
2014-03-19 17:28:57 -0700

    Began working on basic requirements of project.

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,5 +1,7 @@
 var http = require("http");
 var handler = require("./request-handler");
+var helpers = require('./http-helpers');
+var url = require('url');
 
 var port = 8080;
 var ip = "127.0.0.1";
@@ -7,3 +9,18 @@ var server = http.createServer(handler.handleRequest);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
+var routes = {
+  '/': handler.handleRequest
+};
+
+var router = function(req, res) {
+  console.log("Serving request type " + req.method + " for url " + req.url);
+
+  var parsedUrl = url.parse(req.url);
+  var route = routes[parsedUrl.pathname];
+  if (route){
+    route(req, res);
+  } else {
+    helpers.serveAssets(res, null, 404);
+  }
+};
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,58 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var fs = require('fs');
 // require more modules/folders here!
+var helpers = require('./http-helpers');
+
+
+
+var getInfo = function(req, res){
+  var getURL = url.parse(req.url).pathname;
+  fs.readFile(path.join(__dirname, '../archives/sites.txt'), function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    var eachURL = data.toString().split('\n');
+    for (var i=0; i<eachURL.length; i++) {
+      if (getURL === '/' + eachURL[i]) {
+        fs.readFile(path.join(__dirname, '../archives/sites' + getURL), function (err, data) {
+          if (err) {
+            console.log(err);
+          }
+
+          var asset = data.toString();
+          helpers.serveAssets(res, asset, 200);
+        });
+      }
+    }
+  });
+
+  // helpers.serveAssets(res, './public/index.html', 200);
+};
+
+var postInfo = function(req, res){
+  helpers.serveAssets(res, asset, 200);
+};
+
+var options = function(req, res){
+  helpers.serveAssets(res, asset, 200);
+};
+
+var actions = {
+  'GET': getInfo,
+  'POST': postInfo,
+  'OPTIONS': options
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+
+  if (action){
+    action(req, res);
+  } else {
+    helpers.serveAssets(res, null, 404);
+  }
+
+  // res.end(archive.paths.list);
 };
